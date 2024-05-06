f9e56645ed59e8577da4f461fe8dec2d9992a8c6
Max Prokofiev <max.prokofiev@leasoft.org>
2024-05-06 13:59:47 +0300

    Add payment methods deactivation

--- src/Core/ModuleEvents.php
+++ src/Core/ModuleEvents.php
@@ -11,6 +11,7 @@ namespace GingerPayments\Payments\Core;
 
 use Exception;
 use OxidEsales\DoctrineMigrationWrapper\MigrationsBuilder;
+use OxidEsales\Eshop\Core\Field;
 use Symfony\Component\Console\Output\BufferedOutput;
 
 /**
@@ -31,7 +32,18 @@ final class ModuleEvents
     }
     public static function onDeactivate(): void
     {
-
+        $aPayments = [
+            'gingerpaymentscreditcard' => 'Kreditkarte',
+            'gingerpaymentssepa' => 'Lastschrift SEPA'
+        ];
+        $oPayment = oxNew(\OxidEsales\Eshop\Application\Model\Payment::class);
+        foreach ($aPayments as $sPaymentOxid => $sPaymentDesc) {
+            $oPayment->load($sPaymentOxid);
+            if ($oPayment->exists()) {
+                $oPayment->delete();
+            }
+        }
+        unset($oPayment);
     }
 
     /**
@@ -40,5 +52,36 @@ final class ModuleEvents
      */
     public static function addGingerpaymentsPaymentMethods(): void
     {
+        $aPayments = [
+            'gingerpaymentscreditcard'     => [ 'OXID'          => 'gingerpaymentscreditcard',
+                'OXDESC_DE'     => 'Kreditkarte',
+                'OXDESC_EN'     => 'Credit Card',
+                'OXLONGDESC_DE' => 'Der Betrag wird von Ihrer Kreditkarte abgebucht, sobald die Bestellung abgeschickt wird',
+                'OXLONGDESC_EN' => 'The amount will be debited from your credit card once the order is submitted'
+            ],
+            'gingerpaymentssepa'           => [ 'OXID'          => 'gingerpaymentssepa',
+                'OXDESC_DE'     => 'Lastschrift SEPA',
+                'OXDESC_EN'     => 'Direct Debit SEPA',
+                'OXLONGDESC_DE' => 'Ihr Konto wird nach Abschicken der Bestellung belastet',
+                'OXLONGDESC_EN' => 'Your account will be debited upon the order submission'
+            ]
+        ];
+        $oLangArray = \OxidEsales\Eshop\Core\Registry::getLang()->getLanguageArray();
+        $oPayment = oxNew(\OxidEsales\Eshop\Application\Model\Payment::class);
+        foreach ($oLangArray as $oLang) {
+            foreach ($aPayments as $aPayment) {
+                $oPayment->setId($aPayment['OXID']);
+                $oPayment->setLanguage($oLang->id);
+                $sLangAbbr = in_array($oLang->abbr, ['de', 'en']) ? $oLang->abbr : 'en';
+                $oPayment->oxpayments__oxid          = new Field($aPayment['OXID']);
+                $oPayment->oxpayments__oxaddsumrules = new Field('31');
+                $oPayment->oxpayments__oxtoamount    = new Field('1000000');
+                $oPayment->oxpayments__oxtspaymentid = new Field('');
+                $oPayment->oxpayments__oxdesc     = new Field($aPayment['OXDESC_'. strtoupper($sLangAbbr)]);
+                $oPayment->oxpayments__oxlongdesc = new Field($aPayment['OXLONGDESC_'. strtoupper($sLangAbbr)]);
+                $oPayment->save();
+            }
+        }
+        unset($oPayment);
     }
 }
