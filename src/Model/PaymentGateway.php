2710c74eb0780e44b15ba557e9f3d78796fdf8bf
Max Prokofiev <max.prokofiev@leasoft.org>
2024-05-09 02:37:01 +0300

    Create temp order info

--- src/Model/PaymentGateway.php
+++ src/Model/PaymentGateway.php
@@ -2,13 +2,16 @@
 
 namespace GingerPayments\Payments\Model;
 
+use Ginger\ApiClient;
+use http\Client;
 use OxidEsales\EshopCommunity\Application\Model\PaymentGateway as BasePaymentGateway;
 use OxidEsales\Eshop\Core\Field;
 
 
-class PaymentGateway extends  BasePaymentGateway
+class PaymentGateway extends BasePaymentGateway
 {
     private $o_paymentInfo;
+    private $_oOrderInfo;
 
     /**
      * Sets payment parameters.
@@ -18,8 +21,9 @@ class PaymentGateway extends  BasePaymentGateway
     public function setPaymentParams($oUserpayment)
     {
         // store data
-        $this->_oPaymentInfo = & $oUserpayment;
+        $this->_oPaymentInfo = &$oUserpayment;
     }
+
     public function executePayment($dAmount, &$oOrder)
     {
         $this->_iLastErrorNo = null;
@@ -28,11 +32,28 @@ class PaymentGateway extends  BasePaymentGateway
 //        if (!$this->isActive()) {
 //            return true; // fake yes
 //        }
+//        die(var_dump($oOrder));
 
         // proceed with no payment
         // used for other countries
+//        $oOrder = oxNew(\OxidEsales\Eshop\Application\Model\Order::class);
+
+
         if (@$this->_oPaymentInfo->oxuserpayments__oxpaymentsid->value === 'gingerpaymentscreditcard') {
-            die('success');
+            $currency[] = $oOrder->getOrderCurrency();
+
+           $order = [
+               "currency" => $currency[0]->name,
+               "amount" => $dAmount * 10,
+               "description" => 'Order with id: '.$oOrder->getId(),
+               "return_url" => 'http://localhost/en/Vehicles/',
+               "transactions" => [
+                   "payment_method" => "credit-card"
+               ]
+           ];
+//            $orderCreated = $api->createOrder($order);
+
+           die(print_r($orderCreated, true));
         }
 
         return false;
