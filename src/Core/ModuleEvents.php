0c609a2ea1f5d89e17135876c9e84b3e423bdcaa
Max Prokofiev <max.prokofiev@leasoft.org>
2024-05-16 23:06:35 +0300

    Fix deleting payment after deactivation

--- src/Core/ModuleEvents.php
+++ src/Core/ModuleEvents.php
@@ -30,9 +30,10 @@ final class ModuleEvents
     {
         self::addGingerpaymentsPaymentMethods();
     }
+
     public static function onDeactivate(): void
     {
-       self::removeGingerpaymentsPaymentMethods();
+        self::removeGingerpaymentsPaymentMethods();
     }
 
     /**
@@ -42,15 +43,15 @@ final class ModuleEvents
     public static function addGingerpaymentsPaymentMethods(): void
     {
         $aPayments = [
-            'gingerpaymentscreditcard'     => [ 'OXID'          => 'gingerpaymentscreditcard',
-                'OXDESC_DE'     => 'Kreditkarte',
-                'OXDESC_EN'     => 'Credit Card',
+            'gingerpaymentscreditcard' => ['OXID' => 'gingerpaymentscreditcard',
+                'OXDESC_DE' => 'Kreditkarte',
+                'OXDESC_EN' => 'Credit Card',
                 'OXLONGDESC_DE' => 'Der Betrag wird von Ihrer Kreditkarte abgebucht, sobald die Bestellung abgeschickt wird',
                 'OXLONGDESC_EN' => 'The amount will be debited from your credit card once the order is submitted'
             ],
-            'gingerpaymentssepa'           => [ 'OXID'          => 'gingerpaymentssepa',
-                'OXDESC_DE'     => 'Lastschrift SEPA',
-                'OXDESC_EN'     => 'Direct Debit SEPA',
+            'gingerpaymentssepa' => ['OXID' => 'gingerpaymentssepa',
+                'OXDESC_DE' => 'Lastschrift SEPA',
+                'OXDESC_EN' => 'Direct Debit SEPA',
                 'OXLONGDESC_DE' => 'Ihr Konto wird nach Abschicken der Bestellung belastet',
                 'OXLONGDESC_EN' => 'Your account will be debited upon the order submission'
             ]
@@ -62,32 +63,31 @@ final class ModuleEvents
                 $oPayment->setId($aPayment['OXID']);
                 $oPayment->setLanguage($oLang->id);
                 $sLangAbbr = in_array($oLang->abbr, ['de', 'en']) ? $oLang->abbr : 'en';
-                $oPayment->oxpayments__oxid          = new Field($aPayment['OXID']);
+                $oPayment->oxpayments__oxid = new Field($aPayment['OXID']);
                 $oPayment->oxpayments__oxaddsumrules = new Field('31');
-                $oPayment->oxpayments__oxtoamount    = new Field('1000000');
+                $oPayment->oxpayments__oxtoamount = new Field('1000000');
                 $oPayment->oxpayments__oxtspaymentid = new Field('');
-                $oPayment->oxpayments__oxdesc     = new Field($aPayment['OXDESC_'. strtoupper($sLangAbbr)]);
-                $oPayment->oxpayments__oxlongdesc = new Field($aPayment['OXLONGDESC_'. strtoupper($sLangAbbr)]);
+                $oPayment->oxpayments__oxdesc = new Field($aPayment['OXDESC_' . strtoupper($sLangAbbr)]);
+                $oPayment->oxpayments__oxlongdesc = new Field($aPayment['OXLONGDESC_' . strtoupper($sLangAbbr)]);
                 $oPayment->save();
             }
         }
         unset($oPayment);
     }
+
     public static function removeGingerpaymentsPaymentMethods(): void
     {
-        {
-            $aPayments = [
-                'gingerpaymentscreditcard',
-                'gingerpaymentssepa'
-            ];
-            $oPayment = oxNew(\OxidEsales\Eshop\Application\Model\Payment::class);
-            foreach ($aPayments as $sPaymentOxid => $sPaymentDesc) {
-                $oPayment->load($sPaymentOxid);
-                if ($oPayment->exists()) {
-                    $oPayment->delete();
-                }
+        $aPayments = [
+            'gingerpaymentscreditcard',
+            'gingerpaymentssepa'
+        ];
+        $oPayment = oxNew(\OxidEsales\Eshop\Application\Model\Payment::class);
+        foreach ($aPayments as $sPaymentOxid) {
+            if ($oPayment->load($sPaymentOxid)) {
+                $oPayment->delete();
             }
-            unset($oPayment);
         }
+        unset($oPayment);
     }
+
 }
