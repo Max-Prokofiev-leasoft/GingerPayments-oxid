1768b31c83fd8f19565dd9d7f19561a4f4352e60
Max Prokofiev <max.prokofiev@leasoft.org>
2024-05-31 02:52:03 +0300

    Add shiping OrderLinesBuilder

--- src/Builders/OrderLinesBuilder.php
+++ src/Builders/OrderLinesBuilder.php
@@ -8,7 +8,6 @@ use GingerPluginSdk\Properties\Amount;
 use GingerPluginSdk\Properties\Currency;
 use GingerPluginSdk\Properties\VatPercentage;
 use OxidEsales\EshopCommunity\Application\Model\Order as OxidOrder;
-use OxidEsales\EshopCommunity\Application\Model\Article;
 
 class OrderLinesBuilder
 {
@@ -26,7 +25,6 @@ class OrderLinesBuilder
         $lines = [];
 
         foreach ($orderArticles as $orderArticle) {
-            /** @var Article $article */
             $article = $orderArticle->getArticle();
 
             $discountRateValue = null;
@@ -48,8 +46,49 @@ class OrderLinesBuilder
 
             $lines[] = $line;
         }
+
+        if ($order->oxorder__oxdelcost->value > 0) {
+            $lines[] = self::getShippingOrderLine($order);
+        }
+
         return new OrderLines(...$lines);
     }
 
+    /**
+     * Creates a shipping order line.
+     *
+     * @param OxidOrder $order
+     * OXID Order
+     * @return Line
+     * - SDK Line object for shipping
+     */
+    protected static function getShippingOrderLine(OxidOrder $order): Line
+    {
+        $shippingAmount = (float)(
+        $order->oxorder__oxdelcost->value
+        );
 
+        return new Line(
+            type: 'shipping_fee',
+            merchantOrderLineId: 'Shipping',
+            name: self::getShippingName($order),
+            quantity: 1,
+            amount: new Amount((int)($shippingAmount * 100)),
+            vatPercentage: new VatPercentage((int)(0)),
+            currency: new Currency($order->getOrderCurrency()->name)
+        );
+    }
+
+    /**
+     * Retrieves the shipping name from the order.
+     *
+     * @param OxidOrder $order
+     * OXID Order
+     * @return string
+     * - Shipping name
+     */
+    protected static function getShippingName(OxidOrder $order): string
+    {
+        return preg_replace("/[^A-Za-z0-9 ]/", "", $order->oxorder__oxdeltype->value);
+    }
 }
