0903711496daecc033c31c4864f4fd5de8d0e447
Max Prokofiev <max.prokofiev@leasoft.org>
2024-05-31 11:15:45 +0300

    Update PaymentGateway

--- src/Model/PaymentGateway.php
+++ src/Model/PaymentGateway.php
@@ -10,11 +10,21 @@ use GingerPluginSdk\Exceptions\APIException;
 use OxidEsales\EshopCommunity\Application\Model\Order as OxidOrder;
 use OxidEsales\EshopCommunity\Core\Registry;
 
+/**
+ * Class PaymentGateway
+ *
+ * This class is responsible for handling the execution of payments based on the selected payment method.
+ * It supports multiple payment methods and integrates with the Ginger Payments SDK.
+ */
 class PaymentGateway
 {
-
     private array $paymentMethods;
 
+    /**
+     * Initializes the PaymentGateway class.
+     *
+     * The constructor loads the PSP configuration file and initializes the supported payment methods.
+     */
     public function __construct()
     {
         require_once PSPConfig::AUTOLOAD_FILE;
@@ -30,10 +40,10 @@ class PaymentGateway
      * Sets payment parameters.
      *
      * @param object $userPayment User payment object
+     * @return void
      */
     public function setPaymentParams(object $userPayment): void
     {
-        // store data
         $this->paymentInfo = &$userPayment;
     }
 
@@ -41,18 +51,18 @@ class PaymentGateway
      * Executes payment based on the selected payment method.
      *
      * @param float $amount Payment amount
-     * @param OxidOrder $order Order object
-     * @return bool True on successful execution, false otherwise
+     * @param OxidOrder $order OXID Order
+     * @return bool
+     * - True on successful execution
      * @throws APIException
      */
     public function executePayment(float $amount, OxidOrder $order): bool
     {
-
         $paymentId = @$this->paymentInfo->oxuserpayments__oxpaymentsid->value;
 
         if (isset($this->paymentMethods[$paymentId])) {
             $paymentMethod = $this->paymentMethods[$paymentId];
-            $paymentUrl = $paymentMethod->handlePayment(amount:$amount,order: $order);
+            $paymentUrl = $paymentMethod->handlePayment(amount: $amount, order: $order);
             Registry::getSession()->setVariable('payment_url', $paymentUrl);
         }
         return true;
